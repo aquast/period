423e29b6297993520970ea595696b0a4c3f50a2b
Andres Quast <quast@hbz-nrw.de>
2020-07-24 15:03:13 +0200

    reworked layout and usability

--- WebContent/js/filter.js
+++ WebContent/js/filter.js
@@ -5,23 +5,27 @@ $(document).ready(function() {
 
 	$('button.showValid').click(function() {
 		$('.nonValid').fadeTo('slow', 0.2);
-		$('.hiddenValid').fadeTo('slow', 1.0);
 		$('.nonValid').toggleClass('hiddenValid');
 	});
 
 	$('button.showText').click(function() {
-		$('.container,.audio,.video,.3d,.data,.image,.vector,.gps').fadeTo('slow', 0.2);
-		$('.container,.audio,.video,.3d,.data,.image,.vector,.gps').setClass('hidden');
+		$('.container,.audio,.video,.3d,.data,.image,.vector,.gps,.interactive,.publishing,.word,.presentation').fadeTo('slow', 0.2);
+		$('.container,.audio,.video,.3d,.data,.image,.vector,.gps,.interactive,.publishing,.word,.presentation').setClass('hidden');
+	});
+
+	$('button.showDoc').click(function() {
+		$('.container,.audio,.video,.3d,.data,.image,.vector,.gps,.interactive,.text').fadeTo('slow', 0.2);
+		$('.container,.audio,.video,.3d,.data,.image,.vector,.gps,.interactive,.text').setClass('hidden');
 	});
 
 	$('button.showMedia').click(function() {
 		$('.text,.word,.publishing,.presentation,.container,.3d,.data,.image,.vector,.gps').fadeTo('slow', 0.2);
-		$('.text,.word,.publishing,.presentation,.container,.3d,.data,.image,.vector,.gps').setClass('hidden');
+		$('.text,.word,.publishing,.presentation,.container,.3d,.data,.image,.vector,.gps').addClass('hidden');
 	});
 	
 	$('button.showImage').click(function() {
-		$('.text,.word,.publishing,.presentation,.container,.audio,.video,.data,.gps,.3d').fadeTo('slow', 0.2);
-		$('.text,.word,.publishing,.presentation,.container,.audio,.video,.data,.gps,.3d').setClass('hidden');
+		$('.text,.word,.publishing,.presentation,.container,.audio,.video,.data,.gps,.3d,.interactive').fadeTo('slow', 0.2);
+		$('.text,.word,.publishing,.presentation,.container,.audio,.video,.data,.gps,.3d,.interactive').addClass('hidden');
 	});
 
 	$('button.resetAll').click(function() {
