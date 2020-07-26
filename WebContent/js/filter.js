c7f5ac623bda029568a1a3799eea97d23978dfad
Andres Quast <quast@hbz-nrw.de>
2020-07-26 17:20:48 +0200

    new mimetypes added

--- WebContent/js/filter.js
+++ WebContent/js/filter.js
@@ -4,26 +4,34 @@
 $(document).ready(function() {
 
 	$('button.showValid').click(function() {
-		$('.nonValid').fadeTo('slow', 0.2);
+		$('.nonValid').fadeTo('slow', 0.2).attr('status', "hiddenNonValid");
 		$('.nonValid').toggleClass('hiddenValid');
 	});
 
 	$('button.showText').click(function() {
+		$('.element').removeClass('hidden');
+		$("div.element[status!='hiddenNonValid']").fadeTo('slow', 1.0);
 		$('.container,.audio,.video,.3d,.data,.image,.vector,.gps,.interactive,.publishing,.word,.presentation').fadeTo('slow', 0.2);
-		$('.container,.audio,.video,.3d,.data,.image,.vector,.gps,.interactive,.publishing,.word,.presentation').setClass('hidden');
+		$('.container,.audio,.video,.3d,.data,.image,.vector,.gps,.interactive,.publishing,.word,.presentation').addClass('hidden');
 	});
 
 	$('button.showDoc').click(function() {
+		$('.element').removeClass('hidden');
+		$("div.element[status!='hiddenNonValid']").fadeTo('slow', 1.0);
 		$('.container,.audio,.video,.3d,.data,.image,.vector,.gps,.interactive,.text').fadeTo('slow', 0.2);
-		$('.container,.audio,.video,.3d,.data,.image,.vector,.gps,.interactive,.text').setClass('hidden');
+		$('.container,.audio,.video,.3d,.data,.image,.vector,.gps,.interactive,.text').addClass('hidden');
 	});
 
 	$('button.showMedia').click(function() {
+		$('.element').removeClass('hidden');
+		$("div.element[status!='hiddenNonValid']").fadeTo('slow', 1.0);
 		$('.text,.word,.publishing,.presentation,.container,.3d,.data,.image,.vector,.gps').fadeTo('slow', 0.2);
 		$('.text,.word,.publishing,.presentation,.container,.3d,.data,.image,.vector,.gps').addClass('hidden');
 	});
 	
 	$('button.showImage').click(function() {
+		$('.element').removeClass('hidden');
+		$("div.element[status!='hiddenNonValid']").fadeTo('slow', 1.0);
 		$('.text,.word,.publishing,.presentation,.container,.audio,.video,.data,.gps,.3d,.interactive').fadeTo('slow', 0.2);
 		$('.text,.word,.publishing,.presentation,.container,.audio,.video,.data,.gps,.3d,.interactive').addClass('hidden');
 	});
